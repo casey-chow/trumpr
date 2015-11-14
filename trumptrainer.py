90c653f3806fc40820ac9131a7cbb10f50379b4b
matthewli1997 <matthewli1997@gmail.com>
2015-11-14 15:21:27 -0500

    organized text files, changed trainer

--- trumptrainer.py
+++ trumptrainer.py
@@ -2,7 +2,7 @@ from collections import *
 import random
 import string
 
-default_order = 8
+default_order = 9
 
 def train_char_lm(fname, order=default_order):
     data = file(fname).read()
@@ -28,8 +28,6 @@ def generate_letter(lm, history, order):
 
 def generate_text(lm, order, seed="~~~~~~~~", nletters=200):
     history = seed
-    # for i in range(order):
-    #     history += random.choice(string.letters) * order
     out = [seed]
     for i in xrange(nletters):
         c = generate_letter(lm, history, order)
@@ -37,56 +35,44 @@ def generate_text(lm, order, seed="~~~~~~~~", nletters=200):
         out.append(c)
     return "".join(out)
 
-trumpTextSource = "trumpText2.txt"
-lm = train_char_lm(trumpTextSource, order=default_order)
+tweetSeparate = '`'
+def generate_tweets(textSource, textOut, num):
+    trumpTextSource = textSource
+    lm = train_char_lm(trumpTextSource, order=default_order)
 
 
-g = open(trumpTextSource, 'r')
-f = open("trumpresults2.txt", 'a')
+    g = open(trumpTextSource, 'r')
+    f = open(textOut, 'w')
 
-trumptext = g.read()
+    trumptext = g.read()
+
+    for i in range(num):
+        randIndex = int(random.random() * (len(trumptext) - 100))
+        start = 0
+        for j in range(randIndex,len(trumptext)):
+            c = trumptext[j]
+            if c == tweetSeparate:
+                for k in range(j+1, len(trumptext)):
+                    if (trumptext[k].isalpha() or trumptext[k] == '@') and trumptext[k+1] != '.':
+                        start = k
+                        break
+                break
+
+        inputSeed = trumptext[start:start+default_order]
+        resultTweets = generate_text(lm, default_order, inputSeed)
+
+        for j in range(len(resultTweets)-1,0,-1):
+            c = resultTweets[j]
+            if c == tweetSeparate:
+                end = j
+                break
+
+        print(resultTweets[:end])
+        print('')
+        f.write(resultTweets[:end])
+        f.write("\n")
+        f.write("\n")
 
 
-for i in range(30):
-    randIndex = int(random.random() * (len(trumptext) - 100))
-    start = 0
-    for j in range(randIndex,len(trumptext)):
-        c = trumptext[j]
-        if c == '.' or c == '?' or c == '!':
-            for k in range(j+1, len(trumptext)):
-                if trumptext[k].isalpha() or trumptext[k] == '@':
-                    start = k
-                    break
-            break
-
-    inputSeed = trumptext[start:start+default_order]
-    resultTweets = generate_text(lm, default_order, inputSeed)
-
-    for j in range(len(resultTweets)-1,0,-1):
-        c = resultTweets[j]
-        if c == '.' or c == '?' or c == '!':
-            end = j+1
-            break
-
-    print(resultTweets[:end])
-    print()
-    f.write(resultTweets[:end])
-    f.write("\n")
-    f.write("\n")
-
-# seed = 'hel'
-# counter = 0
-# while(counter < 100):
-#     rand = random.uniform(0,1)
-#     probCounter = 0
-    
-#     for char, prob in train_char_lm("trumptext.txt"):
-#         if (rand < probCounter and rand + prob > probCounter):
-#             seed = seed + char
-#             break
-#         else:
-#             probCounter += prob
-#     counter += 1
-# print(seed)
-
+generate_tweets("trumpText3.txt", "trumpresults3.txt", 20)
 
