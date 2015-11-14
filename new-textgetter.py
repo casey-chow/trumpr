8cbd0e7950dd2aeb0fb10304426ff3775bd30d3d
Evan Wildenhain <mew2@princeton.edu>
2015-11-14 15:23:55 -0500

    new parser + sample file with graves (`) to mark end of tweet

--- new-textgetter.py
+++ new-textgetter.py
@@ -1,6 +1,9 @@
 import json, sys, codecs, re
 from twitter import Twitter, OAuth, TwitterHTTPError
-
+if len(sys.argv) > 1:
+    screenname = sys.argv[1]
+else:
+    screenname = 'realDonaldTrump'
 apikey = 'QKeGwK39iGzWLSzcBPRN038vH'
 apisecret = 'WcAtHvSDY7UkBgA1o8VgSOIeDqctnhncy49VXUkasFWEFfhqjw'
 accesstoken = '624850224-yRga3n3qzgyRChfO95sDmEe9cpSlnQg75FRfQuVg'
@@ -11,19 +14,19 @@ oauth = OAuth(accesstoken, accesstokensecret, apikey, apisecret)
 twitter = Twitter(auth=oauth)
 smallestID = '962046407113732096'
 
-outfile = codecs.open('workfile.txt', encoding='utf-8', mode='w')
+outfile = codecs.open('trumpText3.txt', encoding='utf-8', mode='w')
 
 for i in range(19):
     data = twitter.statuses.user_timeline(trim_user='true', max_id=smallestID, 
-		include_rts='false', user_id='25073877', 
-		screen_name='realDonaldTrump', lang='en', count=200)
+		include_rts='false', screen_name=screenname, lang='en', count=200)
     for x in data:
         toWrite = x['text']
         smallestID = x['id']
         if toWrite[0] == '"':
             continue
-        toWrite = toWrite.replace('&amp;', '&')+' '
-        toWrite = re.sub(r'https?://t.co/\w+ ', ' ', toWrite)
-        toWrite = re.sub(r'https?://t.co/\w+"', '', toWrite)
+        toWrite = toWrite.replace('&amp;', '&')
+        toWrite = toWrite.replace('.@', '@')
+        toWrite = re.sub(r'https?://t.co/\w+', '', toWrite)
+        toWrite = re.sub(r'[\s]+', ' ', toWrite) + '` '
         outfile.write(toWrite)
 outfile.close()
