dce4b321f6dc48310ce16224874faeee94d1c2c3
Evan Wildenhain <mew2@princeton.edu>
2015-11-14 13:14:11 -0500

    grab trump's tweets
    outputs trump's 3200 most recent tweets stripped of URLs and special
    formatting and concatted with spaces

---
+++ new-textgetter.py
@@ -0,0 +1,25 @@
+import json, sys, codecs, re
+from twitter import Twitter, OAuth, TwitterHTTPError
+
+apikey = 'QKeGwK39iGzWLSzcBPRN038vH'
+apisecret = 'WcAtHvSDY7UkBgA1o8VgSOIeDqctnhncy49VXUkasFWEFfhqjw'
+accesstoken = '624850224-yRga3n3qzgyRChfO95sDmEe9cpSlnQg75FRfQuVg'
+accesstokensecret = 'GGK6Wa6cdlGe1zxOe19zpuEr7WFolqlNtL1tDpsUth29M'
+
+oauth = OAuth(accesstoken, accesstokensecret, apikey, apisecret)
+
+twitter = Twitter(auth=oauth)
+smallestID = '962046407113732096'
+
+outfile = codecs.open('workfile.txt', encoding='utf-8', mode='w')
+
+for i in range(19):
+    data = twitter.statuses.user_timeline(trim_user='true', max_id=smallestID, 
+		include_rts='false', user_id='25073877', 
+		screen_name='realDonaldTrump', lang='en', count=200)
+    for x in data:
+        toWrite = x['text'].replace('&amp;', '&')+' '
+        toWrite = re.sub(r'https?://t.co/\w+ ', ' ', toWrite)
+        outfile.write(toWrite)
+        smallestID = x['id']
+outfile.close()
