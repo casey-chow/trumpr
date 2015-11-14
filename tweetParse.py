c0330ab45ece4a2ff89e12a7acb3087b68073e86
matthewli1997 <matthewli1997@gmail.com>
2015-11-14 12:49:42 -0500

    updated trump texts

--- tweetParse.py
+++ tweetParse.py
@@ -43,23 +43,24 @@ for i in range(19):
 	print(smallestID)
 
 
-# for line in tweets_file:
+# for line in trumpTweets:
 #     try:
+#     	print(line)
 #         # Read in one line of the file, convert it into a json object 
 #         tweet = json.loads(line.strip())
 #         if 'text' in tweet: # only messages contains 'text' field is a tweet
 #             print tweet['id'] # This is the tweet's id
-#             print tweet['created_at'] # when the tweet posted
+#             # print tweet['created_at'] # when the tweet posted
 #             print tweet['text'] # content of the tweet
                         
-#             print tweet['user']['id'] # id of the user who posted the tweet
-#             print tweet['user']['name'] # name of the user, e.g. "Wei Xu"
-#             print tweet['user']['screen_name'] # name of the user account, e.g. "cocoweixu"
+#             # print tweet['user']['id'] # id of the user who posted the tweet
+#             # print tweet['user']['name'] # name of the user, e.g. "Wei Xu"
+#             # print tweet['user']['screen_name'] # name of the user account, e.g. "cocoweixu"
 
-#             hashtags = []
-#             for hashtag in tweet['entities']['hashtags']:
-#             	hashtags.append(hashtag['text'])
-#             print hashtags
+#             # hashtags = []
+#             # for hashtag in tweet['entities']['hashtags']:
+#             # 	hashtags.append(hashtag['text'])
+#             # print hashtags
 
 #     except:
 #         # read in a line is not in JSON format (sometimes error occured)
