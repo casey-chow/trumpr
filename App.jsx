df0ac12e583356cb907a6d2b75ebb56639ca9f1e
Casey Chow <casey.chow@gmail.com>
2015-11-15 03:08:33 -0500

    remove routes

--- App.jsx
+++ App.jsx
@@ -3,8 +3,9 @@ App = React.createClass({
         return (
             <div className="container">
                 <Navigation />
+                <Header />
                 <main className="row">
-                    {this.props.children}
+                    <Tweets className="col-md-6 col-md-offset-3" author="realDonaldTrump" />
                 </main>
             </div>
         );
--- Tweets.jsx
+++ Tweets.jsx
@@ -1,38 +1,28 @@
 Tweets = React.createClass({
-    
-    // This mixin makes the getMeteorData method work
+
     mixins: [ReactMeteorData],
-    
-    // Loads items from the TweetDB collection and puts them on this.data.tweetdb
+
     getMeteorData() {
         return {
-            tweetdb: TweetDB.find({}).fetch()
+          tweets: TweetsStore.find({ authorHandle: this.props.author }, { sort: { timestamp: -1, limit: 10 }}).fetch()
         }
     },
-    
-    defaultTweets: [
-        'Cras justo odio',
-        'Dapibus ac facilisis in',
-        'Morbi leo risus',
-        'Porta ac consectetur ac',
-        'Vestibulum at eros'
-    ],
 
-    renderTweets(tweets) {
-        return tweets.map(function (tweet, key) {
+    renderTweets() {
+        Meteor.call('getTweets', 10, this.props.author);
+        return this.data.tweets.map(function (tweet, key) {
             return <li className="list-group-item" key={key}>{tweet.text}</li>;
         });
     },
 
     render() {
-        console.log(this.defaultTweets);
         return (
             <section {...this.props} >
                 <header className="page-header">
-                    <h1>Tweets <small>@{this.props.params.author}</small></h1>
+                    <h1>Tweets <small>@{this.props.author}</small></h1>
                 </header>
                 <ul className="list-group">
-                    {this.renderTweets(this.data.tweetdb)}
+                    {this.renderTweets()}
                 </ul>
             </section>
         );
