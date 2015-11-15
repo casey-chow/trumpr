8a5b6cb7058cbd2b33bdd6c6a1d2a635868e85a3
Casey Chow <casey.chow@gmail.com>
2015-11-14 20:39:15 -0500

    split nav elements into diff files

---
+++ Navigation.jsx
@@ -0,0 +1,37 @@
+Navigation = React.createClass({
+    render() {
+        return (
+            <nav className="navbar navbar-default navbar-fixed-top">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">Need More Donald?</a>
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="navbar-collapse">
+                        <ul className="nav navbar-nav">
+                            <li><a href="/a/theRealDonaldTrump">Donald Trump <span className="sr-only">(current)</span></a></li>
+                            <li><a href="#">Carly Fiorina</a></li>
+                        </ul>
+                        {/* 
+                            <ul className="nav navbar-nav navbar-right">
+                            <li><a href="#">About</a></li>
+                            </ul> 
+                        */}
+                        <form className="navbar-form navbar-right" id="custom-handle">
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Twitter Handle" />
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </nav>
+        );
+    }
+});
