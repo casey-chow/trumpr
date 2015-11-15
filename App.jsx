570812f48e5ebe1e29fbc3755797bb1754a4aebf
Evan Wildenhain <mew2@princeton.edu>
2015-11-14 21:46:45 -0500

    using exec to call the python script and put the tweets in the db

--- App.jsx
+++ App.jsx
@@ -6,7 +6,6 @@ App = React.createClass({
                 <Header />
                 <main className="row">
                     <Tweets className="col-md-4" author="realDonaldTrump" />
-                    <Speeches className="col-md-8" author="Donald Trump" />
                 </main>
             </div>
         );
--- more_donald.jsx
+++ more_donald.jsx
@@ -1,42 +1,43 @@
-Console = new Mongo.Collection('console');
+// Define a collection to hold the tweets
+TweetDB = new Mongo.Collection("tweetdb");
 
 if (Meteor.isClient) {
+    
     Meteor.startup(function() {
         ReactDOM.render(<App />, document.getElementById("render-target"));
     });
-
-	var exec = Npm.require('child_process').exec;
-	var Fiber = Npm.require('fibers');
-	var Future = Npm.require('fibers/future');
-	var tweets = [];
-	
-    Meteor.methods({
-
- 	 callPython: function() {
-    	var fut = new Future();
-    	
-    	exec('python ./politicianTweetGenerator.py 10 realDonaldTrump', function (error, stdout, stderr) {
-    		stdout.on('readable', function() {
-    			tweets[tweets.length-1] = stdout.read();
-    		})
-
-	      // // if you want to write to Mongo in this callback
-	      // // you need to get yourself a Fiber
-
-	      // new Fiber(function() {
-	      //   ...
-	      //   fut.return('Python was here');
-	      // }).run();
-
-	    });
-	    return fut.wait();
-	  },
-
-	});
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function () {
+    var exec = Npm.require('child_process').exec
+    getTweets = function(num, screenName) {
+        // 1st line name, 2nd line screen name, 3rd line profile image url
+            exec('python ./politicianTweetGenerator.py '+num+' '+ screenName, function (error, stdout, stderr) {
+                var buf = stdout.toString();
+                var arr = buf.split("\n");
+                var name = arr[0];
+                var picURL = arr[2];
+                var i = 3;
+                while (i < arr.length) {
+                    TweetDB.insert({
+                        name: name,
+                        text: arr[i], 
+                        screenName: screenName,
+                        picURL: picURL
+                    });
+                }
 
+                // // if you want to write to Mongo in this callback
+                // // you need to get yourself a Fiber
+
+                // new Fiber(function() {
+                //   ...
+                //   fut.return('Python was here');
+                // }).run();
+
+            });
+        }
+    Meteor.startup(function () {
+        getTweets(10, 'realDonaldTrump')
     });
 }
