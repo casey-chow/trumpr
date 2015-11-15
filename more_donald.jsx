7255294321b37c9bc6d3e2a4394b17780c9a667a
Casey Chow <casey.chow@gmail.com>
2015-11-14 20:49:32 -0500

    switch to routes

--- more_donald.jsx
+++ more_donald.jsx
@@ -1,6 +1,14 @@
+const {Router, Route} = ReactRouter;
+
 if (Meteor.isClient) {
     Meteor.startup(function() {
-        ReactDOM.render(<App />, document.getElementById("render-target"));
+        var routes = (
+            <Router>
+                <Route path="/" component={App}>
+                </Route>
+            </Router>
+        );
+        ReactDOM.render(routes, document.getElementById("render-target"));
     });
 }
 
